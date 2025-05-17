177350a522492bb8e70b93bac5dcf2d8def7df68
rZcld7 <russellzuleikha6@gmail.com>
2025-05-17 16:48:22 -0400

    se agrego la autenticacion y usuario en firebase (autenticacion)

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CerrarSesion from '../components/CerrarSesion';
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Bienvenido a EcoFood</h2>
+      <p>Esta es la página de inicio de tu aplicación.</p>
+      <CerrarSesion />
+    </div>
+  );
+}
