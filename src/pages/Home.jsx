9e742547036e7ba52edb55e06d8d09190aee1e67
ArredondoVM21 <arredondoborquez2174@gmail.com>
2025-05-19 14:26:32 -0400

    actualización del Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CerrarSesion from '../components/CerrarSesion';
+import { getUserData } from "../services/userService";
+import { useAuth } from "../context/AuthContext";
 
 export default function Home() {
+  const { user } = useAuth();
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const datos = await getUserData(user.uid);
+      setUserData(datos);
+    };
+    if (user) fetch();
+  }, [user]);
+
   return (
     <div>
       <h2>Bienvenido a EcoFood</h2>
